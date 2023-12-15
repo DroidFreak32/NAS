b8b8f48d45ca12b89dab1d628f53e616b571d904
DroidFreak32 <rushabshah32@gmail.com>
2023-12-15 23:08:16 +0000

    containers: misc: Import Themepark theme on few ctrs

---
+++ scripts/docker_vol_ctr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+volumes=$(docker volume ls  --format '{{.Name}}')
+
+for volume in $volumes
+do
+  echo $volume
+  docker ps -a --filter volume="$volume"  --format '{{.Names}}' | sed 's/^/  /'
+done
